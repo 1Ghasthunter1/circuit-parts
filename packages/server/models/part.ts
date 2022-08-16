8f695ee5839dc668005ba526da0596dc7d21cb11
Hunter Pruett <hunterpruett2003@gmail.com>
2022-08-16 07:05:54 +0000

    Schema ready for work tmrw

--- packages/server/models/part.ts
+++ packages/server/models/part.ts
@@ -31,7 +31,9 @@ const partSchema = new mongoose.Schema({
 });
 
 partSchema.set("toJSON", {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   transform: (_document: any, returnedObject: any) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
