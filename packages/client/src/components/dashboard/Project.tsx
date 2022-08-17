c141b46556bfc1f5568e9fffb85b2e8845b0d002
Hunter Pruett <hunterpruett2003@gmail.com>
2022-08-17 07:57:36 +0000

    Fully working projects

--- packages/client/src/components/dashboard/Project.tsx
+++ packages/client/src/components/dashboard/Project.tsx
@@ -7,7 +7,7 @@ interface ProjectTypes {
 
 const ProjectComponent = ({ project }: ProjectTypes) => {
   return (
-    <div className="flex flex-row items-center transition bg-gray-200 my-2 p-4 rounded-md hover:bg-gray-300 hover:scale-105">
+    <div className="group flex flex-row items-center transition bg-gray-200 my-2 p-4 rounded-md hover:bg-gray-300 hover:scale-105">
       <div className="h-full items-center float-left p-1 mx-4">
         <FontAwesomeIcon icon="folder-open" size="2x" color="#EE3D96" />
       </div>
@@ -17,6 +17,9 @@ const ProjectComponent = ({ project }: ProjectTypes) => {
           <b>{project.prefix}</b>
         </p>
         <p className="text-sm">{project.description}</p>
+      </div>
+      <div className="hidden transition group-hover:block ml-auto mt-auto items-bottom p-1">
+        <FontAwesomeIcon icon="trash" color="#c70404" />
       </div>
     </div>
   );
