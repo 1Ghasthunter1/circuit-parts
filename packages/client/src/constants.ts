9f5bd02f68e24f2112f6f8d0a9eba1473f1028b6
Hunter Pruett <hunterpruett2003@gmail.com>
2022-09-01 03:15:00 +0000

    Fixed config

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,12 +1,17 @@
+type NODE_ENV_TYPES = "development_vm" | "development_local" | "production";
+
 const getApiBaseUrl = () => {
-  switch (import.meta.env.MODE) {
+  switch (import.meta.env.VITE_NODE_ENV as NODE_ENV_TYPES) {
     case "production":
       return "/api";
-    case "development":
-      return `${import.meta.env.BASE_URL}api`;
+    case "development_local":
+      return "/api";
+    case "development_vm":
+      return "http://localhost:3001/api";
     default:
-      console.log(import.meta.env.MODE);
-      break;
+      throw new Error(
+        `VITE_NODE_ENV is not configured directly. It should be 'development_vm', 'development_local', or 'production', not ${import.meta.env.VITE_NODE_ENV as NODE_ENV_TYPES}`
+      );
   }
 };
 export const apiBaseUrl = getApiBaseUrl();
