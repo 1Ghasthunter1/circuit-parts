d81bbdf247c39a3fb28ca7349765018df0bc75d1
Hunter Pruett <hunterpruett2003@gmail.com>
2022-08-31 20:02:50 -0700

    Added frontend api url auto config

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,1 +1,12 @@
-export const apiBaseUrl = "/api";
+const getApiBaseUrl = () => {
+  switch (import.meta.env.MODE) {
+    case "production":
+      return "/api";
+    case "development":
+      return `${import.meta.env.BASE_URL}api`;
+    default:
+      console.log(import.meta.env.MODE);
+      break;
+  }
+};
+export const apiBaseUrl = getApiBaseUrl();
