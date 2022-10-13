44ed2a33434fb9c401a3edb65119def3a5240442
Hunter Pruett <hunterpruett2003@gmail.com>
2022-10-12 18:16:32 -0700

    table styled

--- packages/client/src/components/orders/projectOrders/OrdersTable.tsx
+++ packages/client/src/components/orders/projectOrders/OrdersTable.tsx
@@ -21,6 +21,7 @@ const columnHelper = createColumnHelper<Order>();
 const columns = [
   columnHelper.display({
     header: "Name",
+    id: "name",
     cell: ({ row }) => {
       const order = row.original;
       return (
@@ -68,16 +69,20 @@ const OrdersTable = ({ orders }: { orders: Order[] }) => {
       <div className="mt-8 flex flex-col">
         <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle">
-            <div className="overflow-hidden shadow ring-1 rounded ring-black ring-opacity-5 md:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-gray-50">
+            <div className="overflow-hidden shadow ring-1 ring-opacity-5 m-1 rounded ring-black  md:rounded-lg">
+              <table className="min-w-full">
+                <thead className="bg-white">
                   {table.getHeaderGroups().map((headerGroup) => (
                     <tr key={headerGroup.id}>
                       {headerGroup.headers.map((header) => (
                         <th
                           key={header.id}
                           scope="col"
-                          className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                          className={
+                            header.index === 0
+                              ? "py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                              : "px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                          }
                         >
                           {header.isPlaceholder
                             ? null
@@ -92,8 +97,8 @@ const OrdersTable = ({ orders }: { orders: Order[] }) => {
                 </thead>
                 {table.getGroupedRowModel().rows.map((group) => {
                   return (
-                    <tbody className="divide-y divide-gray-200 bg-white">
-                      <tr>
+                    <tbody className="bg-white">
+                      <tr className="border-t border-gray-200">
                         <th
                           colSpan={table.getVisibleFlatColumns().length}
                           className="bg-gray-50 px-4 py-2 text-left text-sm font-semibold text-gray-900 sm:px-6"
@@ -101,24 +106,36 @@ const OrdersTable = ({ orders }: { orders: Order[] }) => {
                           {group.original.vendor}
                         </th>
                       </tr>
-                      {group.getLeafRows().map((row) => {
-                        return (
-                          <tr key={row.id} className="hover:bg-gray-50">
-                            {row.getVisibleCells().map((cell) => {
-                              return (
-                                <td
-                                  key={cell.id}
-                                  className="whitespace-nowrap px-3 py-1 text-sm text-gray-900"
-                                >
-                                  {flexRender(cell.column.columnDef.cell, {
-                                    ...cell.getContext(),
-                                  })}
-                                </td>
-                              );
-                            })}
-                          </tr>
-                        );
-                      })}
+                      {group.getLeafRows().map((row) => (
+                        <tr
+                          key={row.id}
+                          className={`
+                            ${
+                              row.index === 0
+                                ? "border-gray-300 border-t"
+                                : "border-gray-200 border-t"
+                            } hover:bg-gray-100
+                          `}
+                        >
+                          {row.getVisibleCells().map((cell) => {
+                            console.log(cell.column.id);
+                            return (
+                              <td
+                                key={cell.id}
+                                className={
+                                  cell.column.id === "name"
+                                    ? "whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-700 sm:pl-6"
+                                    : "whitespace-nowrap px-3 py-2 text-sm text-gray-500"
+                                }
+                              >
+                                {flexRender(cell.column.columnDef.cell, {
+                                  ...cell.getContext(),
+                                })}
+                              </td>
+                            );
+                          })}
+                        </tr>
+                      ))}
                     </tbody>
                   );
                 })}
